3f32264c3996c9541551b005511c4e375d766354
vlw0052 <leon@vlw2.com>
2019-08-26 19:12:12 -0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { CamerasComponent } from './cameras/cameras.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'vehicles'
+  },
+  {
+    path: 'vehicles',
+    component: VehiclesComponent
+  },
+  {
+    path: 'cameras',
+    component: CamerasComponent
+  },
+  {
+    path: 'assignments',
+    component: AssignmentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { CamerasComponent } from './cameras/cameras.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiclesComponent,
+    CamerasComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
