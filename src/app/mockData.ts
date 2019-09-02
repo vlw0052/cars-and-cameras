ead3472f47ca362f92955dd9c47c2be154c24256
vlw0052 <leon@vlw2.com>
2019-09-02 00:58:03 -0500

    Added mock data

---
+++ src/app/mockData.ts
@@ -0,0 +1,159 @@
+import { Camera, Vehicle } from './models';
+
+export const cameraData: Camera[] = [
+  {
+    id: 0,
+    deviceNo: 'bc849'
+  },
+  {
+    id: 1,
+    deviceNo: 'cb933'
+  },
+  {
+    id: 2,
+    deviceNo: 'cb870'
+  },
+  {
+    id: 3,
+    deviceNo: 'bc247'
+  },
+  {
+    id: 4,
+    deviceNo: 'cb720'
+  },
+  {
+    id: 5,
+    deviceNo: 'bc880'
+  },
+  {
+    id: 6,
+    deviceNo: 'cb68'
+  },
+  {
+    id: 7,
+    deviceNo: 'cb875'
+  },
+  {
+    id: 8,
+    deviceNo: 'cb650'
+  },
+  {
+    id: 9,
+    deviceNo: 'cb704'
+  },
+  {
+    id: 10,
+    deviceNo: 'cb669'
+  },
+  {
+    id: 11,
+    deviceNo: 'cb926'
+  },
+  {
+    id: 12,
+    deviceNo: 'bc433'
+  },
+  {
+    id: 13,
+    deviceNo: 'bc294'
+  },
+  {
+    id: 14,
+    deviceNo: 'cb637'
+  },
+  {
+    id: 15,
+    deviceNo: 'bc612'
+  },
+  {
+    id: 16,
+    deviceNo: 'cb811'
+  },
+  {
+    id: 17,
+    deviceNo: 'cb477'
+  },
+  {
+    id: 18,
+    deviceNo: 'bc221'
+  },
+  {
+    id: 19,
+    deviceNo: 'bc858'
+  }
+];
+
+export const vehicleData: Vehicle[] = [
+  {
+    id: 0,
+    name: 'cr-Giles'
+  },
+  {
+    id: 1,
+    name: 'cr-Ruiz'
+  },
+  {
+    id: 2,
+    name: 'cr-Vinson'
+  },
+  {
+    id: 3,
+    name: 'vh-Mckee'
+  },
+  {
+    id: 4,
+    name: 'cr-Mullins'
+  },
+  {
+    id: 5,
+    name: 'vh-Dale'
+  },
+  {
+    id: 6,
+    name: 'vh-Wheeler'
+  },
+  {
+    id: 7,
+    name: 'vh-Vang'
+  },
+  {
+    id: 8,
+    name: 'cr-Bauer'
+  },
+  {
+    id: 9,
+    name: 'vh-Rogers'
+  },
+  {
+    id: 10,
+    name: 'cr-Hendricks'
+  },
+  {
+    id: 11,
+    name: 'vh-Haney'
+  },
+  {
+    id: 12,
+    name: 'vh-Levy'
+  },
+  {
+    id: 13,
+    name: 'cr-Cooley'
+  },
+  {
+    id: 14,
+    name: 'cr-Koch'
+  },
+  {
+    id: 15,
+    name: 'vh-Boyer'
+  },
+  {
+    id: 16,
+    name: 'vh-Riley'
+  },
+  {
+    id: 17,
+    name: 'vh-Golden'
+  }
+];
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Camera } from '../models';
+import { Camera, Vehicle } from '../models';
+import { cameraData, vehicleData } from '../mockData';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,87 +12,9 @@ export class DataService {
       subs.next({ items: cameraData });
     });
   }
-}
-
-export const cameraData = [
-  {
-    id: 0,
-    deviceNo: 'bc849'
-  },
-  {
-    id: 1,
-    deviceNo: 'cb933'
-  },
-  {
-    id: 2,
-    deviceNo: 'cb870'
-  },
-  {
-    id: 3,
-    deviceNo: 'bc247'
-  },
-  {
-    id: 4,
-    deviceNo: 'cb720'
-  },
-  {
-    id: 5,
-    deviceNo: 'bc880'
-  },
-  {
-    id: 6,
-    deviceNo: 'cb68'
-  },
-  {
-    id: 7,
-    deviceNo: 'cb875'
-  },
-  {
-    id: 8,
-    deviceNo: 'cb650'
-  },
-  {
-    id: 9,
-    deviceNo: 'cb704'
-  },
-  {
-    id: 10,
-    deviceNo: 'cb669'
-  },
-  {
-    id: 11,
-    deviceNo: 'cb926'
-  },
-  {
-    id: 12,
-    deviceNo: 'bc433'
-  },
-  {
-    id: 13,
-    deviceNo: 'bc294'
-  },
-  {
-    id: 14,
-    deviceNo: 'cb637'
-  },
-  {
-    id: 15,
-    deviceNo: 'bc612'
-  },
-  {
-    id: 16,
-    deviceNo: 'cb811'
-  },
-  {
-    id: 17,
-    deviceNo: 'cb477'
-  },
-  {
-    id: 18,
-    deviceNo: 'bc221'
-  },
-  {
-    id: 19,
-    deviceNo: 'bc858'
+  getVehicles() {
+    return new Observable<{ items: Vehicle[] }>(subs => {
+      subs.next({ items: vehicleData });
+    });
   }
-];
+}
