6b048b9ce1df7c7aeb3ea412eba265db5a2f53d6
vlw0052 <leon@vlw2.com>
2019-09-02 01:52:29 -0500

    Added assignments to root state

--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,7 +1,9 @@
 import { CameraStoreState } from './cameras-store';
 import { VehicleStoreState } from './vehicles-store';
+import { AssignmentStoreState } from './assignments-store';
 
 export interface RootState {
   cameras: CameraStoreState.State;
   vehicles: VehicleStoreState.State;
+  assignments: AssignmentStoreState.State;
 }
